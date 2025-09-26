7bb9654b1f69c12d249aca983f93cc2ff09c2ad0
ChristienWong <c.a.wong@wustl.edu>
2025-09-26 02:16:19 -0500

    fixed ground and added heart system

---
+++ Assets/Code/Q1/Heart.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Heart : MonoBehaviour
+{
+    public int health;
+    public int numOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    void Update()
+    {
+        if (health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+
+        if (health < 0){
+            health = 0;
+        }
+
+        for (int i = 0; i < hearts.Length; i++){
+            if (hearts[i] == null){
+                continue;
+            }
+
+            if (i < health){
+                hearts[i].sprite = fullHeart;
+            }
+            else{
+                hearts[i].sprite = emptyHeart;
+            }
+
+            hearts[i].enabled = i < numOfHearts;
+        }
+    }
+}
